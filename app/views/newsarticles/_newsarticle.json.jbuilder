d3b30bacb287112fc80bef9b5e6f014a7a3971ca
NCIGareth <x19122748@student.ncirl.ie>
2023-01-12 15:04:32 +0000

    First Commit

---
+++ app/views/newsarticles/_newsarticle.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! newsarticle, :id, :source, :author, :title, :text, :imglink, :articlelink, :created_at, :updated_at
+json.url newsarticle_url(newsarticle, format: :json)
---
+++ db/migrate/20230112150223_create_newsarticles.rb
@@ -0,0 +1,14 @@
+class CreateNewsarticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :newsarticles do |t|
+      t.string :source
+      t.string :author
+      t.string :title
+      t.string :text
+      t.string :imglink
+      t.string :articlelink
+
+      t.timestamps
+    end
+  end
+end
