7d8101082bdfda6dca2ab91c5fc87711f4799146
NCIGareth <x19122748@student.ncirl.ie>
2023-01-12 21:47:44 +0000

    NEWS

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,6 @@
+class WeatherController < ApplicationController
+  def index
+    weather = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?lat=53.3498&lon=6.2603&appid=e24b337541816a7da96e2761e56597a9&units=metric")
+    @weather = JSON.parse(weather.body)
+  end
+end
